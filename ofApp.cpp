0950fef82b35cdcf9831003d6d3e292b9793dd45
stiag0 <stiago01r@gmail.com>
2019-11-10 21:32:47 -0500

    primer commit con  bezier y easycam

---
+++ ofApp.cpp
@@ -0,0 +1,138 @@
+#include "ofApp.h"
+using namespace std;
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofEnableDepthTest();
+	ofSetVerticalSync(true);
+	cam.setDistance(160);
+
+	ofSetWindowTitle("Bezier");
+	gui.setup();
+	gui.add(accuracy.setup("t", 5555, 1, 15000));
+	gui.add(clearBtn.setup("erase"));
+	gui.add(drawBtn.setup("draw"));
+
+	ofSetFrameRate(60);
+	drawPressed = true;
+	//anchor_image.load("targetmin.png");
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	gui.draw();
+	cam.begin();
+	if (clearBtn) {
+		drawPressed = false;
+		controlPts.clear();
+	}
+
+	if (drawBtn)
+		drawPressed = true;
+
+	if (drawPressed) {
+		std::vector<ofVec2f> end = bezier_curve(controlPts, accuracy);
+		if (end.size() > 1) {
+			ofSetColor(ofColor::black);
+			for (unsigned int i = 0; i < controlPts.size() - 1; ++i) {
+				ofDrawLine(controlPts[i].x, controlPts[i].y, controlPts[i + 1].x, controlPts[i + 1].y);
+				anchor_image.draw(controlPts[i].x, controlPts[i].y);
+				ofDrawSphere(controlPts[i].x, controlPts[i].y,100);
+			}
+			ofSetColor(ofColor::blue);
+			for (unsigned int i = 0; i < end.size(); ++i) {
+				ofCircle(end[i].x, end[i].y, 1);
+			}
+		}
+	}
+	
+	cam.end();
+}
+
+//-----------M----------------------------------
+
+float ofApp::interpolate(float pos1, float pos2, float accuracy) {
+	return (1 - accuracy) * pos1 + accuracy * pos2;
+}
+
+vector<ofVec2f> ofApp::bezier_curve(vector<ofVec2f>& anchor, float accuracy) {
+	if (anchor.size() <= 2)
+		return anchor;
+
+	vector<ofVec2f> end;
+
+	for (float i = 0.f; i < accuracy; ++i) {
+		float t = i / accuracy;
+
+		vector<ofVec2f> temp(anchor);
+
+		while (temp.size() > 1) {
+			vector<ofVec2f> temp2;
+			for (unsigned int j = 1; j < temp.size(); ++j)
+				temp2.push_back(ofVec2f(interpolate(temp[j - 1].x, temp[j].x, t),
+					interpolate(temp[j - 1].y, temp[j].y, t)));
+			temp.swap(temp2);
+		}
+		end.push_back(temp.front());
+	}
+	return end;
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+	controlPts.push_back(ofVec2f(x, y));
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
---
+++ ofApp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream> 
+#include "ofxGui.h"
+using namespace std;
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+
+	void setup();
+	void update();
+	void draw();
+
+	ofEasyCam cam;
+
+	float interpolate(float x, float y, float accuracy);
+	vector<ofVec3f> anclas3;
+	vector<ofVec2f> bezier_curve(vector<ofVec2f>& anchor, float accuracy);
+	ofxIntSlider accuracy;
+	ofxButton clearBtn;
+	ofxButton drawBtn;
+	ofxPanel gui;
+	std::vector<ofVec2f> controlPts;
+	bool drawPressed;
+	ofImage anchor_image;
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+};
